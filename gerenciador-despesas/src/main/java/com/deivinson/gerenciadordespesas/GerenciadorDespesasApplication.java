40c2145bc03f5aa97de6bed8f80ffe36b1291a50
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-08-27 17:17:26 -0300

    initial commit

---
+++ gerenciador-despesas/src/main/java/com/deivinson/gerenciadordespesas/GerenciadorDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.deivinson.gerenciadordespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDespesasApplication.class, args);
+	}
+
+}
---
+++ gerenciador-despesas/src/test/java/com/deivinson/gerenciadordespesas/GerenciadorDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deivinson.gerenciadordespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
