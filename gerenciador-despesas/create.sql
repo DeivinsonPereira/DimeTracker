d8b015a79f46cf9fc9924b01aa953642d9c3ca22
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-08-29 17:34:01 -0300

    Dockerfile, docker-compose.yml, images

---
+++ gerenciador-despesas/create.sql
@@ -0,0 +1,24 @@
+
+create table tb_categoria (id  bigserial not null, nome varchar(255), primary key (id));
+create table tb_despesa (id  bigserial not null, data date, valor float8, categoria_id int8, usuario_id int8, primary key (id));
+create table tb_usuario (id  bigserial not null, nome varchar(255), primary key (id));
+alter table tb_despesa add constraint FKdt96ti1mfs6fknqsv1sywb3dn foreign key (categoria_id) references tb_categoria;
+alter table tb_despesa add constraint FK7segqqfdh448r7g6mdthagf8g foreign key (usuario_id) references tb_usuario;
+INSERT INTO tb_categoria (nome) VALUES ('Restaurante');
+INSERT INTO tb_categoria (nome) VALUES ('Faculdade');
+INSERT INTO tb_categoria (nome) VALUES ('Carro');
+INSERT INTO tb_categoria (nome) VALUES ('Aluguel');
+INSERT INTO tb_usuario (id, nome) VALUES (1, 'Paulo Padrão');
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (70.0, '2023-07-12',1,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (40.75, '2023-08-10',1,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (150.12, '2023-08-17',1,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (178.49, '2023-08-27',1,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (850.0, '2023-06-15',2,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (850.0, '2023-07-15',2,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (850.0, '2023-08-15',2,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (700.0, '2023-06-10',3,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (700.0, '2023-07-10',3,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (700.0, '2023-08-10',3,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (1200.0, '2023-06-10',4,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (1200.0, '2023-07-10',4,1);
+INSERT INTO tb_despesa (valor, data, categoria_id, usuario_id) VALUES (1200.0, '2023-08-10',4,1);
