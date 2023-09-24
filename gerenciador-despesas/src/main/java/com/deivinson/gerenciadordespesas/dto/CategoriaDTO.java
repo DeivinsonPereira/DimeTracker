aaad1887102d44a1159b27b5beebe2908f069d8a
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-09-24 10:52:12 -0300

    CategoriaDTO ajustado

--- gerenciador-despesas/src/main/java/com/deivinson/gerenciadordespesas/dto/CategoriaDTO.java
+++ gerenciador-despesas/src/main/java/com/deivinson/gerenciadordespesas/dto/CategoriaDTO.java
@@ -1,10 +1,6 @@
 package com.deivinson.gerenciadordespesas.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.deivinson.gerenciadordespesas.entities.Categoria;
-import com.deivinson.gerenciadordespesas.entities.Despesa;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +12,6 @@ public class CategoriaDTO {
 	private Long id;
 	private String nome;
 	
-	private List<DespesaDTO> despesas = new ArrayList<>();
-
 	public CategoriaDTO(Long id, String nome) {
 		this.id = id;
 		this.nome = nome;
@@ -28,13 +22,4 @@ public class CategoriaDTO {
 		this.nome = entity.getNome();
 	}
 	
-	public CategoriaDTO(Categoria entity, List<Despesa> despesas) {
-		this(entity);
-		despesas.forEach(desp -> this.despesas.add(new DespesaDTO(desp)));
-		
-	}
-
-	@SuppressWarnings("unused")
-	private void setDespesas(List<DespesaDTO> despesaDTO) {
-	}
 }
